75218959daa68bcebc8e16f4937916ed41c6c8fe
unknown <kearaabigaelburbos@gmail.com>
2025-07-20 12:23:25 +0000

    second commit

--- savings.c
+++ savings.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
-// This program does not include leap years.
+// Is it a leap year? Answer 'yes' and ignore if not.
+char* leapYear = "-";
+
 
 int main() {
-    double amountBeingSavedPerDay = 2500;
-    int count = 1;
+    double amountBeingSavedPerDay = 2500; // Input the amount you wish to save everyday.
     double amountInAYear = 0;
+    double amountInAYear2 = 0;
     
-    while (count <= 12) {
-        amountInAYear = amountBeingSavedPerDay * 365;
-        count++;
+    if (strcmp(leapYear, "yes") == 0) {
+        amountInAYear = amountBeingSavedPerDay * 366;
+        printf("If you save $%.2lf a day, you would have $%.2lf in a year.", amountBeingSavedPerDay, amountInAYear);
+} else {
+        amountInAYear2 = amountBeingSavedPerDay * 365;
+        printf("If you save $%.2lf a day, you would have $%.2lf in a year.", amountBeingSavedPerDay, amountInAYear2);
     }
-    printf("If you save $%.2lf a day, you would have $%.2lf in a year.", amountBeingSavedPerDay, amountInAYear);
     return 0;
-}
+    }
